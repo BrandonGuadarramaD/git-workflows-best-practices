47987280f4dac0f51d359c0c9e75bfb67f39b108
BrandonGuadarramaD <brandon.guadarrama@digitalfemsa.com>
2024-10-30 14:55:41 -0600

    Add project java
    Add base project

---
+++ src/main/java/com/b2b/lab1/Lab1Application.java
@@ -0,0 +1,15 @@
+package com.b2b.lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		//SpringApplication.run(Lab1Application.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/b2b/lab1/Lab1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.b2b.lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class Lab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
